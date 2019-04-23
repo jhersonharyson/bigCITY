5ae98f94bdda9c36b2fc860a1035c0bed2850138
j.haryson <jherson_k-f@hotmail.com>
2019-04-23 00:19:53 -0300

    remoção de comentarios

--- src/controller/iot.js
+++ src/controller/iot.js
@@ -2,18 +2,8 @@ const axios = require('axios')
 const constrants = require('./../bin/constants')
 const Iot = require('../models/iot')
 exports.post = ('/', async (req, res, next) => {    
-    // recuperar dados 
-    // tempo real
     try {
-        // TODO: get request modify
-        //res.status(200).send({"received": req.body})
-        // TODO: get request
-        // const geolocation = req.body.geolocation
-        ///// TODO: remove 
         const {lat, lon, speed, sensor} = req.body
-        ///// TODO: remove
-        //const lat = req.body.lat//'52.41072'
-        //const log = req.body.log//'4.84239'
         const data = new Date()
         console.log(data)
         
@@ -65,30 +55,6 @@ exports.post = ('/', async (req, res, next) => {
         console.log({ error: e })
         res.send({ error: e })
     }
-    // sensor:{
-    //     temperature: Number,
-    //     humidity: Number,
-    //     noise_pollution: Number,
-    //     toxic_gases:{
-    //         co: Number,
-    //         smoke: Number,
-    //         lta: Number,
-    //     }
-    // }
-
-    // current_wheater = {
-    //     temperature: Number,
-    //     wind_direction: String,
-    //     wind_velocity: Number,
-    //     humidity: Number,
-    //     condition: String,
-    //     pressure: Number,
-    //     icon: String,
-    //     sensation_current: Number,
-    // }
-    // requisições de trafego
-    // salvar no banco
-    // res.send({ ok: "ok" })
 })
 
 exports.test = (req, res, next) => {
