9e8c4a48d92c3aa639011ac870fb3308f78ba5e5
j.haryson <jherson_k-f@hotmail.com>
2019-04-23 09:40:08 -0300

    adição do analytics - media controlada

--- src/routes/analytics.js
+++ src/routes/analytics.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 
-router.get('/analytics', require('../controller/analytics'))
+router.get('/analytics', require('../controller/analytics').mean_analytics)
+
+router.get('/analytics/:limit', require('../controller/analytics').mean_analytics_limit)
 
 module.exports =  router
